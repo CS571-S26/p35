9bfa768b428cad57717e5c125b3f808414e140c1
pseigne <71709682+pseigne@users.noreply.github.com>
2026-03-18 22:09:17 -0500

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -6,12 +6,22 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [dots, setDots] = useState(1)
 
+
+useEffect(() => {
+  const interval = setInterval(() => {
+    setDots((prev) => (prev < 3 ? prev + 1 : 0));
+  }, 500); // adjust speed (ms)
+
+  return () => clearInterval(interval); // cleanup
+}, []);
+ 
   return (
     <>
       <section id="center">
         <div>
-          <h1>Project Loading...</h1>
+          <h1>Project Loading{'.'.repeat(dots)}</h1>
           <p>
             <code>Pierce Seigne & Ryan Ross</code>
           </p>
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/p35',
+  base: '/p35/',
   plugins: [react()],
   build: {
-    outDir: "docs"  // GitHub Pages default folder
+    outDir: "docs"  // For GitHub Pages
   }
 })
