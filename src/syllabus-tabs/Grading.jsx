50cdd22e3dda2ccc243459cc014c2af5439680ca
pseigne <71709682+pseigne@users.noreply.github.com>
2026-04-01 20:28:18 -0400

    set up grading tab

--- src/syllabus-tabs/Grading.jsx
+++ src/syllabus-tabs/Grading.jsx
@@ -3,5 +3,8 @@ import { useContext } from "react";
 import AllSyllabiContext from "../context/AllSyllabiContext";
 
 export default function Grading() {
+	const { allSyllabi } = useContext(AllSyllabiContext);
+	const index = (sessionStorage.getItem('currentSyllabusIndex'));
+	const currentSyllabus = allSyllabi[index];
 	return <h1>Grading TODO</h1>;
 }
